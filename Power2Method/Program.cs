9ad4a9045363a026261c647f7bb39ccf53588a52
AntonioLVieira <antonio.allvieira@gmail.com>
2023-03-22 22:25:50 +0000

    Ex.5 - Adicionei comentários XML aos métodos e à Classe

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -2,20 +2,30 @@
 
 namespace Power2Method
 {
+    /// <summary>
+    /// Programa que calcula as potências de 2
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Chama o método
+        /// </summary>
+        /// <param name="args">Lê da consola</param>
         static void Main(string[] args)
         {
-            // Chamamos método PowersOf2Until5()
-            PowersOf2Until5();
-            
+            PowersOf2Until5();  
         }
-        // Criar método
+        /// <summary>
+        /// Calcula as potências de 2 dos números 1 a 5 e imprime na consola
+        /// </summary>
         private static void PowersOf2Until5()
         {
            PowersOf2UntiLN(5);
         }
-        // Criar método
+        /// <summary>
+        /// Calcula as potências de 2 dos números 1 a n e imprime na consola
+        /// </summary>
+        /// <param name="n">último número a calcular a potência</param>
         private static void PowersOf2UntiLN(int n)
         {
             for (int i = 1; i <= (1 << n); i = i << 1)
